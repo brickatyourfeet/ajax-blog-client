a335d4f4545f2136e1a07afb373a85634c30cb8f
Wes Reid <bwreid@gmail.com>
2017-11-01 22:35:34 -0700

    Hijack back button

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 const localhostURL = 'http://localhost:3000/posts'
 const herokuURL = 'https://ajax-blog-api.herokuapp.com/posts'
 const baseURL = window.location.href.includes('localhost') ? localhostURL : herokuURL
+
 homeView.init()
+
+window.onhashchange = () => homeView.init()
 
 document.querySelector('#create-post').addEventListener('click', event => {
   newPostView.init()
